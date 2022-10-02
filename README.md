a855653d6d0d4ec12f046afc37a255c653eab91a
Lanka Vasanth <elvee963@gmail.com>
2022-10-03 01:59:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Spectra
+
+![](https://tipseason.com/assets/images/hacktoberfest-banner.jpg)
+
+🟥🟧🟨🟩🟦🟪🟫⬛⬜ 
+
+## About
+
+As a frequenter of the internet, you must often come across interesting colors 
+that you would want to use later, in your own project.
+
+This program captures the input of your mouse, and follows your cursor, and when clicked at a certain point
+the program returns the RGB and hex value of the color on that particular pixel.
+
+## Tech Stack
+
+Python 3
+
+## Run spectra
+
+- Clone the repo.  `git clone https://github.com/IEEE-VIT/spectra.git`  && cd spectra
+
+- Create a virtual environment 
+
+- Activate your virtual environment
+
+- ` pip install -r requirements.txt `
+
+- ` python3 main.py `
+
+## Getting Started
+
+To get started:
+
+- Clone the repo.
+  `git clone https://github.com/IEEE-VIT/spectra.git`
+- Checkout to a new branch.
+  `git checkout -b my-amazing-feature`
+- Make some amazing changes.
+- `git add .`
+- `git commit -m "<verb> : <action>."`
+- `git push origin my-amazing-feature`
+- Open a pull request :)
+
+
+
+## Contributing 
+
+To start contributing, check out [CONTRIBUTING.md](). New contributors are always welcome to support this project.
+Kindly consider leaving a ⭐ if you like the project :)
+Later versions of the project would involve building a GUI around the appliocation using TKinter, or any other library of your convenience.
+
+## License
+
+This project is licensed under [MIT](https://github.com/IEEE-VIT/spectra/blob/master/LICENSE.md).
---
+++ main.py
@@ -0,0 +1,37 @@
+from pynput import keyboard
+from pynput import mouse
+from PIL import Image, ImageGrab
+
+# These are the dependencies we will be using, we use pynput to record the input from either the mouse or the keyboard,
+# The cursor coordinates are used to capture the pixel the cursor is resting on, and whether or not the mouse has been clicked.
+
+# Function to get the hex code value which takes a tuple containing the red,green,blue values from 0-255.
+def getHex(rgb):
+    pass
+
+# The getColor function accepts 2 arguments, 1 x coordinate, 1 y coordinate, we capture or "grab" an image,
+# and based on the x-y coordinates we get the color at that particular pixel. 
+def getColor(x,y):
+    pass
+
+# Function to record whether or not the mouse has been clicked, takes x, y coordinates as arguments,
+#  and button specifies which particular botton is pressed(in our case, it would be 'right click'),
+#  and press is a boolean indicating if it has been pressed or not.
+def onClick(x,y,button,press):
+    pass
+
+# Since we cannot keep the script running all the time,
+#  and it will only tell us the value of the color if we press the close button,
+#  we'll need to code an exit in some way. So we use a key on the keyboard to terminate the program.
+def onRel(key):
+    pass
+
+# The main function that runs, to listen for keyboard, mouse inputs.
+def main():
+    with keyboard.Listener(on_release=onRel) as k:
+        with mouse.Listener(on_click=onClick) as m:
+            k.join()
+            m.join()    
+
+if __name__ =="__main__":
+    main()
