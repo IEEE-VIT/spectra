51635a774df6954bf283b5bca61e0a4761a8e05f
codehero7386 <56253286+codehero7386@users.noreply.github.com>
2023-10-11 22:30:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,76 +3,52 @@ from pynput import mouse
 from PIL import Image, ImageGrab
 from os.path import isfile
 
-# These are the dependencies we will be using, we use pynput to record the input from either the mouse or the keyboard,
-# The cursor coordinates are used to capture the pixel the cursor is resting on, and whether or not the mouse has been clicked.
-
 colorList = []
 
-# Function to print the color detected
-# Assuming it is stored hex code
-# colorList is a global variable
 def printColorList():
-    print("Colors detected are:", end=" ")
-    for color in colorList:
-        print(f"#{color}", end=" ")
-    print()
+    if colorList:
+        print("Colors detected:")
+        for color in colorList:
+            print(f"#{color}")
+    else:
+        print("No colors detected.")
 
-# Function to export the colors detected to file_path
-# Assume that global colorList stores hexcodes of colors
-# If file_path is already present it raises Error
 def exportToFile(file_path):
     if isfile(file_path):
         raise FileExistsError(f"{file_path} is already present")
     with open(file_path, "w") as f:
         for color in colorList:
             f.write(f"#{color}\n")
+        print(f"Colors exported to {file_path}")
 
-# Function to get the hex code value which takes a tuple containing the red,green,blue values from 0-255.
 def getHex(rgb):
-    output = ''
-    
-    for value in rgb :
-        output += hex(value)[2:].upper().zfill(2)
-        
-    return output
+    return ''.join(hex(value)[2:].upper().zfill(2) for value in rgb)
 
-# Function to convert hex code to RGB format
 def hex_to_rgb(hexcode):
-    # Remove the '#' if present
     hexcode = hexcode.lstrip('#')
-    
-    # Convert hex to RGB
-    rgb = tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4))
-    return rgb
+    return tuple(int(hexcode[i:i+2], 16) for i in (0, 2, 4))
 
-# The getColor function accepts 2 arguments, 1 x coordinate, 1 y coordinate, we capture or "grab" an image,
-# and based on the x-y coordinates we get the color at that particular pixel. 
-def getColor(x,y):
-    coor = x,y
-    
+def getColor(x, y):
+    coor = x, y
     return ImageGrab.grab().getpixel(coor)
 
-# Function to record whether or not the mouse has been clicked, takes x, y coordinates as arguments,
-#  and button specifies which particular botton is pressed(in our case, it would be 'right click'),
-#  and press is a boolean indicating if it has been pressed or not.
-def onClick(x,y,button,press):
-    pass
+def onClick(x, y, button, press):
+    if press:  # Check if the mouse button is pressed
+        color = getColor(x, y)
+        hex_color = getHex(color)
+        colorList.append(hex_color)
 
-# Since we cannot keep the script running all the time,
-#  and it will only tell us the value of the color if we press the close button,
-#  we'll need to code an exit in some way. So we use a key on the keyboard to terminate the program.
 def onRel(key):
-    #Setting Delete key as the exit key.
-    if (key==keyboard.Key.delete):
-        #Stopping the Listener.
+    if key == keyboard.Key.delete:
         return False
 
-# The main function that runs, to listen for keyboard, mouse inputs.
 def main():
+    print("Color Picker Tool")
+    print("Press the Delete key to stop capturing colors.")
     with keyboard.Listener(on_release=onRel) as k:
         with mouse.Listener(on_click=onClick) as m:
             k.join()
-            m.join()    
+            m.join()
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
